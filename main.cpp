52b587e038b6b72a1ca3be6d2cdb7004b8b755fc
Simon Janezic <simon.janezic@gmail.com>
2021-12-25 09:46:53 +0100

    Basic CMake setup with Boost program_options library

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include <boost/program_options.hpp>
+
+namespace po = boost::program_options;
+
+int main(int argc, char** argv) {
+    // Declare the supported options.
+    boost::program_options::options_description desc("Allowed options");
+    desc.add_options()
+            ("help", "produce help message");
+
+    po::variables_map vm;
+    po::store(po::parse_command_line(argc, argv, desc), vm);
+    po::notify(vm);
+
+    if (vm.count("help")) {
+        std::cout << desc << "\n";
+        return 1;
+    }
+}
