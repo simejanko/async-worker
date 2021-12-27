37d684e808f4a3105b5d26d3b1c35615078fb724
Simon Janezic <simon.janezic@gmail.com>
2021-12-27 10:26:46 +0100

    Start implementing safe worker pause/restart

--- worker.cpp
+++ worker.cpp
@@ -7,22 +7,50 @@ Worker::~Worker() {
 }
 
 void Worker::pause() {
+    std::unique_lock<std::mutex> lock(status_m_);
+    if (status_ == Status::PAUSED) {
+        return;
+    }
 
+    if (status_ != Status::RUNNING) {
+        throw std::logic_error("Worker must be running or paused.");
+    }
+
+    status_change_ = Status::PAUSED;
+
+    // wait for pause to happen
+    status_cv_.wait(lock, [this]() { return status_ == Status::PAUSED; });
 }
 
 void Worker::restart() {
+    std::unique_lock<std::mutex> lock(status_m_);
+    if (status_ == Status::RUNNING) {
+        return;
+    }
 
+    if (status_ != Status::PAUSED) {
+        throw std::logic_error("Worker is not paused or running.");
+    }
+
+    status_change_ = Status::RUNNING;
+    status_cv_.notify_one(); //TODO: check if this is ok, without manually unlocking prior
+
+    // wait for restart to happen
+    status_cv_.wait(lock, [this]() { return status_ == Status::RUNNING; });
 }
 
 void Worker::stop() {
-
 }
 
-void Worker::set_progress(int progress) {
+bool Worker::update(int progress) {
     if (progress < 0 || progress > 100) {
         throw std::domain_error("Progress is outside the 0-100 range");
     }
     progress_ = progress;
+
+    //TODO: handle status change, waking from paused, etc...
+
+    return false;
 }
 
 std::ostream& operator<<(std::ostream& os, Worker& worker) {
--- worker.h
+++ worker.h
@@ -3,9 +3,13 @@
 
 #include <thread>
 #include <iostream>
+#include <atomic>
+#include <condition_variable>
+#include <optional>
 
 /**
  * Abstract class for async workers implemented with std::thread_ that can be safely paused, restarted, stopped and destroyed.
+ * Subclasses should implement work() method and regularly call update() method inside (see method docstrings for details).
  */
 class Worker {
 public:
@@ -27,7 +31,10 @@ public:
     Worker& operator=(Worker& other) = delete;
 
     /** @return worker status (e.g. running, paused, ...) */
-    [[nodiscard]] Status status() const { return status_; }
+    [[nodiscard]] Status status() const {
+        std::lock_guard<std::mutex> lock(status_m_);
+        return status_;
+    }
 
     /** @return number to identify this worker by */
     [[nodiscard]] int id() const { return id_; }
@@ -36,40 +43,46 @@ public:
     [[nodiscard]] int progress() const { return progress_; };
 
     /**
-     * Pauses worker
+     * Pauses worker (blocking call)
      * @throws std::logic_error if worker is not in running or paused state when the method is called
      */
     void pause();
 
     /**
-    * Restarts (resumes) worker
+    * Restarts (resumes) worker (blocking call)
     * @throws std::logic_error if worker is not in running or paused state when the method is called
     */
     void restart();
 
     /**
-    * Stops worker. Worker can't be restarted after it is stopped.
+    * Stops worker (blocking call). Worker can't be restarted after it is stopped
     * @throws std::logic_error if worker has already finished it's work (finished state)
     */
     void stop();
 
 protected:
     /**
-     * Sets worker's progress, in the 0-100 range (percentage)
+     * Publishes worker's progress, sleeps in case worker should be paused and checks if worker needs to stop.
+     * Good worker implementations should should call this regularly (inside work method)
+     * while still keeping in mind the overhead of this call
+     * @param progress worker's updated progress, in the 0-100 range (percentage)
+     * @return boolean indicating whether the worker should cleanly stop (true) or keep running (false)
      * @throws std::domain_error if progress passed is not in the 0-100 range
      */
-    void set_progress(int progress);
+    bool update(int progress);
 
 private:
     /** Where the actual work that's to be run in separate thread is implemented. */
     virtual void work() = 0;
 
     int id_ = -1; // TODO: should it be part of the class? What's its use here?
-    std::thread thread_;
     Status status_ = Status::RUNNING;
+    std::atomic<int> progress_ = 0; // in percentages (0-100)
 
-    int progress_ = 0; // in percentages (0-100)
-
+    std::thread thread_; // worker thread
+    std::optional<Status> status_change_; // scheduled status change
+    mutable std::mutex status_m_; // mutex for accessing worker status
+    mutable std::condition_variable status_cv_; // conditional variable for changing worker status
 };
 
 std::ostream& operator<<(std::ostream& os, Worker& worker);
