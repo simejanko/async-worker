6f4385454b0f2a736c4406b63474fcbfba257616
Simon Janezic <simon.janezic@gmail.com>
2021-12-28 07:26:53 +0100

    Add the rest of safe worker pause/restart/stop

--- worker.cpp
+++ worker.cpp
@@ -3,54 +3,75 @@
 Worker::Worker(int id) : id_(id) {}
 
 Worker::~Worker() {
-
+    try {
+        stop();
+    } catch (std::logic_error& e) { // worker has already finished, swallow
+    }
 }
 
 void Worker::pause() {
     std::unique_lock<std::mutex> lock(status_m_);
-    if (status_ == Status::PAUSED) {
-        return;
-    }
-
     if (status_ != Status::RUNNING) {
-        throw std::logic_error("Worker must be running or paused.");
+        throw std::logic_error("Worker must be running to preform pause action");
     }
 
     status_change_ = Status::PAUSED;
 
-    // wait for pause to happen
-    status_cv_.wait(lock, [this]() { return status_ == Status::PAUSED; });
+    // wait for pause to happen or for worker to finish
+    status_cv_.wait(lock, [this]() { return status_ == Status::PAUSED || status_ == Status::FINISHED; });
 }
 
 void Worker::restart() {
     std::unique_lock<std::mutex> lock(status_m_);
-    if (status_ == Status::RUNNING) {
-        return;
-    }
-
     if (status_ != Status::PAUSED) {
-        throw std::logic_error("Worker is not paused or running.");
+        throw std::logic_error("Worker must be paused to preform restart action");
     }
 
     status_change_ = Status::RUNNING;
     status_cv_.notify_one(); //TODO: check if this is ok, without manually unlocking prior
 
-    // wait for restart to happen
-    status_cv_.wait(lock, [this]() { return status_ == Status::RUNNING; });
+    // wait for restart to happen or for worker to finish
+    status_cv_.wait(lock, [this]() { return status_ == Status::RUNNING || status_ == Status::FINISHED; });
 }
 
 void Worker::stop() {
+    std::unique_lock<std::mutex> lock(status_m_);
+    if (status_ != Status::RUNNING && status_ != Status::PAUSED) {
+        throw std::logic_error("Worker must be running or paused to preform stop action");
+    }
+
+    status_change_ = Status::STOPPED;
+    status_cv_.notify_one();
+    lock.unlock();
+
+    thread_.join();
 }
 
-bool Worker::update(int progress) {
+bool Worker::status_update() {
+    std::unique_lock<std::mutex> lock(status_m_);
+    if (status_change_ == Status::PAUSED) {
+        status_ = Status::PAUSED;
+        status_change_.reset();
+        // notify of the status change
+        status_cv_.notify_one();
+        // sleep until restart or stop is requested
+        status_cv_.wait(lock,
+                        [this]() { return status_change_ == Status::RUNNING || status_change_ == Status::STOPPED; });
+    }
+
+    if (status_change_ == Status::STOPPED) {
+        status_change_.reset();
+        return false; // worker implementation needs to stop cleanly
+    }
+
+    return true;
+}
+
+void Worker::set_progress(int progress) {
     if (progress < 0 || progress > 100) {
         throw std::domain_error("Progress is outside the 0-100 range");
     }
     progress_ = progress;
-
-    //TODO: handle status change, waking from paused, etc...
-
-    return false;
 }
 
 std::ostream& operator<<(std::ostream& os, Worker& worker) {
--- worker.h
+++ worker.h
@@ -9,7 +9,8 @@
 
 /**
  * Abstract class for async workers implemented with std::thread_ that can be safely paused, restarted, stopped and destroyed.
- * Subclasses should implement work() method and regularly call update() method inside (see method docstrings for details).
+ * Subclasses should implement work() method and regularly call status_update() method inside (see method docstrings for details).
+ * It is assumed the Worker object is controlled from a single thread.
  */
 class Worker {
 public:
@@ -44,39 +45,45 @@ public:
 
     /**
      * Pauses worker (blocking call)
-     * @throws std::logic_error if worker is not in running or paused state when the method is called
+     * @throws std::logic_error if worker is not running when the method is called
      */
     void pause();
 
     /**
     * Restarts (resumes) worker (blocking call)
-    * @throws std::logic_error if worker is not in running or paused state when the method is called
+    * @throws std::logic_error if worker is not paused when the method is called
     */
     void restart();
 
     /**
     * Stops worker (blocking call). Worker can't be restarted after it is stopped
-    * @throws std::logic_error if worker has already finished it's work (finished state)
+    * @throws std::logic_error if worker has already finished it's work
     */
     void stop();
 
 protected:
     /**
-     * Publishes worker's progress, sleeps in case worker should be paused and checks if worker needs to stop.
+     * Sleeps in case worker should be paused and checks if worker needs to stop.
      * Good worker implementations should should call this regularly (inside work method)
-     * while still keeping in mind the overhead of this call
-     * @param progress worker's updated progress, in the 0-100 range (percentage)
+     * while still keeping in mind the overhead of this call (most notably the mutex lock)
      * @return boolean indicating whether the worker should cleanly stop (true) or keep running (false)
+     */
+    bool status_update();
+
+    /**
+     * Publishes worker's progress.
+     * Good worker implementations should should call this regularly (inside work method).
+     * @param progress worker's updated progress, in the 0-100 range (percentage)
      * @throws std::domain_error if progress passed is not in the 0-100 range
      */
-    bool update(int progress);
+    void set_progress(int progress);
 
 private:
     /** Where the actual work that's to be run in separate thread is implemented. */
     virtual void work() = 0;
 
     int id_ = -1; // TODO: should it be part of the class? What's its use here?
-    Status status_ = Status::RUNNING;
+    Status status_ = Status::RUNNING; // TODO: think about using atomic for this and status_change_ and relieving some of the locking
     std::atomic<int> progress_ = 0; // in percentages (0-100)
 
     std::thread thread_; // worker thread
